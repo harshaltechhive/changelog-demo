adf72f533fcc54314f4fd02b35546d31366f9d95
harshaltechhive <harshil@techhive.co.in>
2026-02-27 15:15:48 +0530

    some changes in counter

--- src/Counter.js
+++ src/Counter.js
@@ -5,6 +5,15 @@ function increase() {
   console.log("Count:", count);
 }
 
+function multiply() {
+  count *= 2;
+  console.log("Count:", count);
+}
+function divide() {
+  count /= 2;
+  console.log("Count:", count);
+}
+
 function decrease() {
   count--;
   console.log("Count:", count);
